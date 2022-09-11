c0bbb519843fa165de790742d37ecb81abc05e5d
sabbirzzaman <sztonmoy7@gmail.com>
2022-09-11 17:35:58 +0600

    create api slice and initial rtk setup

---
+++ src/features/auth/authSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {}
+
+const authSlice = createSlice({
+    name: 'api',
+    initialState,
+    reducers: {}
+})
+
+export default authSlice.reducer;
+export const {} = authSlice.actions;
